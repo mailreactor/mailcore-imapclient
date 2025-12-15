e80b78169fd9e5597204fb400fdb7d455973e3f6
Hans Christian von Stockhausen <hans.vonstockhausen@bancon-it.com>
2025-12-15 18:12:03 +0200

    E3S11.0 bootstrap done

---
+++ src/mailcore_imapclient/__init__.py
@@ -0,0 +1,7 @@
+"""mailcore-imapclient - IMAPClient adapter for mailcore."""
+
+__version__ = "0.1.0"
+
+# Exports will be populated in Story 3.11
+# from .adapter import IMAPClientAdapter
+# __all__ = ["IMAPClientAdapter"]
